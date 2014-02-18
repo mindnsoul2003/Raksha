4076bbbecb557f61330656bac71ff89fc5b30ed3
mindnsoul2003 <mindnsoul2003@yahoo.com>
2014-02-18 11:27:18 -0800

    Update SpiritCharacterModule.java

--- Character_Spirit/src/net/sf/anathema/character/spirit/SpiritCharacterModule.java
+++ Character_Spirit/src/net/sf/anathema/character/spirit/SpiritCharacterModule.java
@@ -1,4 +1,4 @@
-package net.sf.anathema.character.spirit;
+package net.sf.anathema.character.raksha;
 
 import net.sf.anathema.character.generic.framework.module.CharacterModule;
 import net.sf.anathema.character.generic.framework.module.CharacterTypeModule;
@@ -6,10 +6,10 @@ import net.sf.anathema.character.generic.type.CharacterType;
 import net.sf.anathema.character.generic.type.ICharacterType;
 
 @CharacterModule
-public class SpiritCharacterModule extends CharacterTypeModule {
+public class RakshaCharacterModule extends CharacterTypeModule {
 
   @Override
   protected ICharacterType getType() {
-	  return CharacterType.SPIRIT;
+	  return CharacterType.RAKSHA;
   }
 }
