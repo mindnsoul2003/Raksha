f1987edb3e428e0bb62cce12f0b44e04f70a96b8
mindnsoul2003 <mindnsoul2003@yahoo.com>
2014-02-18 14:01:10 -0800

    Create RakshaCaste.java

---
+++ Character_Spirit/src/net/sf/anathema/character/raksha/caste/RakshaCaste.java
@@ -0,0 +1,18 @@
+package net.sf.anathema.character.raksha.caste;
+
+
+import net.sf.anathema.character.generic.caste.ICasteType;
+
+
+public enum InfernalCaste implements ICasteType {
+
+
+  Diplomat, Entertainer, Warrior, Worker, Guide;
+
+
+  @Override
+  public String getId() {
+    return name();
+  }
+}
+
