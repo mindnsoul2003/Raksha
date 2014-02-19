f9bf053a459ee13ee4073f90fc8ffd95f0cd2ce2
mindnsoul2003 <mindnsoul2003@yahoo.com>
2014-02-19 08:00:58 -0800

    Update RakshaLureContentFactory.java

--- Character_Raksha/src/net/sf/anathema/character/raksha/reporting/content/RakshaLureContentFactory.java
+++ Character_Raksha/src/net/sf/anathema/character/raksha/reporting/content/RakshaLureContentFactory.java
@@ -1,22 +1,18 @@
 package net.sf.anathema.character.raksha.reporting.content;
 
-
 import net.sf.anathema.character.generic.character.IGenericCharacter;
 import net.sf.anathema.character.reporting.pdf.content.RegisteredReportContent;
 import net.sf.anathema.character.reporting.pdf.content.ReportContentFactory;
 import net.sf.anathema.character.reporting.pdf.content.ReportSession;
 import net.sf.anathema.lib.resources.Resources;
 
-
 @RegisteredReportContent(produces = RakshaLureContent.class)
 public class RakshaLureFactory implements ReportContentFactory<RakshaLureContent> {
   private Resources resources;
 
-
   public RakshaLureFactory(Resources resources) {
     this.resources = resources;
   }
-
 
   @Override
   public RakshaLureContent create(ReportSession session, IGenericCharacter character) {
