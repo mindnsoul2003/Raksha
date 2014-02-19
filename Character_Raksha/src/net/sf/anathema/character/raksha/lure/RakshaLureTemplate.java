f084ff1687b0564fd1de6c0f5c2a61a6c8982765
mindnsoul2003 <mindnsoul2003@yahoo.com>
2014-02-19 08:09:26 -0800

    Create RakshaLureTemplate.java

---
+++ Character_Raksha/src/net/sf/anathema/character/raksha/lure/RakshaLureTemplate.java
@@ -0,0 +1,18 @@
+package net.sf.anathema.character.raksha.lure;
+
+
+import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
+
+
+public class InfernalUrgeTemplate implements IAdditionalTemplate {
+
+
+  public static final String ID = "Raksha.Lure.Template";
+
+
+  @Override
+  public String getId() {
+    return ID;
+  }
+}
+
