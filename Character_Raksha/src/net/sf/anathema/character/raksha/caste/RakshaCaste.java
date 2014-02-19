519143e6ca36b43874a32d75072ae5997e431901
mindnsoul2003 <mindnsoul2003@yahoo.com>
2014-02-19 07:56:04 -0800

    Update RakshaCaste.java

--- Character_Raksha/src/net/sf/anathema/character/raksha/caste/RakshaCaste.java
+++ Character_Raksha/src/net/sf/anathema/character/raksha/caste/RakshaCaste.java
@@ -1,14 +1,10 @@
 package net.sf.anathema.character.raksha.caste;
 
-
 import net.sf.anathema.character.generic.caste.ICasteType;
-
 
 public enum InfernalCaste implements ICasteType {
 
-
   Diplomat, Entertainer, Warrior, Worker, Guide;
-
 
   @Override
   public String getId() {
